3b91b249ba2e6f2b79ce021dba553404277d0829
sacraBlast <rafialayyubi08@gmail.com>
2023-11-06 20:06:14 +0800

    fxml

--- app/src/main/java/org/gnulag/xplora/controllers/Controller.java
+++ app/src/main/java/org/gnulag/xplora/controllers/Controller.java
@@ -1,8 +1,47 @@
 package org.gnulag.xplora.controllers;
 
-/**
- * Controller
- */
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private ListView<?> listView;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private AnchorPane slider;
+
+    @FXML
+    void searchKeyOrValue(ActionEvent event) {
+
+    }
+
+    @FXML
+    void closeSideBar(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
 
 }
