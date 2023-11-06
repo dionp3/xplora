2c079c0787b8ed8b9814d964c8b210fba55072bf
dionp3 <donpr2003@gmail.com>
2023-11-06 18:51:32 +0800

    Controller.java and main.fxml still not good

--- app/src/main/java/org/gnulag/xplora/controllers/Controller.java
+++ app/src/main/java/org/gnulag/xplora/controllers/Controller.java
@@ -1,8 +1,33 @@
-package org.gnulag.xplora.controllers;
+package org.gnulag.xplora;
 
-/**
- * Controller
- */
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import org.gnulag.xplora.models.RedBlackTreeMap;
+import org.gnulag.xplora.utils.JsonUtil;
+import org.gnulag.xplora.utils.PrintsUtil;
+
 public class Controller {
+    @FXML
+    private TextField searchTextField;
 
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private ListView<String> outputListView;
+
+    private RedBlackTreeMap<String, String> rbTree;
+
+    public Controller() {
+        rbTree = new RedBlackTreeMap<>();
+        JsonUtil.loadJsonData(rbTree, "/data.json");
+    }
+
+    @FXML
+    private void handleSearchButton() {
+        String searchParam = searchTextField.getText();
+        PrintsUtil.printCombinedOutput(rbTree, searchParam);
+    }
 }
