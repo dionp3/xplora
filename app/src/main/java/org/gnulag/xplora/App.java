cf8d5336271d9d25b7a1fa0686b63ea26502a0c2
faiz ahnaf samudra azis <fzhnf@root533.premium-rootserver.net>
2023-11-08 02:20:42 +0800

    changes: if else konvensional jadi switch case meribetkan GimmickUtils

--- app/src/main/java/org/gnulag/xplora/App.java
+++ app/src/main/java/org/gnulag/xplora/App.java
@@ -1,36 +1,48 @@
 package org.gnulag.xplora;
 
-import org.gnulag.xplora.controllers.Controller;
-import java.io.IOException;
+import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.gnulag.xplora.controllers.Controller;
+import org.gnulag.xplora.models.RedBlackTreeMap;
+import org.gnulag.xplora.utils.GimmickUtil;
+import org.gnulag.xplora.utils.JsonUtil;
+import org.gnulag.xplora.utils.PrintsUtil;
 
 public class App extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        // Create a label with the "Hello World!" text
-        try {
-            FXMLLoader board = new FXMLLoader(App.class.getResource("/fxml/main.fxml"));
-            Parent root = board.load();
-            Controller controller = board.getController();
-            // Create a scene and set it on the stage
-            Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    // Create a label with the "Hello World!" text
+    try {
+      FXMLLoader board = new FXMLLoader(App.class.getResource("/fxml/main.fxml"));
+      Parent root = board.load();
+      Controller controller = board.getController();
+      // Create a scene and set it on the stage
+      Scene scene = new Scene(root);
+      primaryStage.setScene(scene);
 
-            // Set the title of the application window
-            primaryStage.setTitle("Xplora");
+      // Set the title of the application window
+      primaryStage.setTitle("Xplora");
 
-            // Show the application window
-            primaryStage.show();
+      // Show the application window
+      primaryStage.show();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-    public static void main(String[] args) {
-        // launch(args);
-    }
+  }
+
+  public static void main(String[] args) {
+    // launch(args)
+    RedBlackTreeMap<String, String> rbTree = new RedBlackTreeMap<>();
+    JsonUtil.loadJsonData(rbTree, "/data.json");
+    ArrayList<String> resultByKey = rbTree.searchKeysAndValuesByContainingKey("suit");
+    ArrayList<String> resultByValue = rbTree.searchKeysAndValuesByContainingValue("suit");
+    ArrayList<String> combinedOutput = PrintsUtil.combineResults(resultByKey, resultByValue);
+    GimmickUtil.checkGimmick(combinedOutput);
+    System.out.println(combinedOutput);
+  }
 }
