4798ef715f96c420cfcedf09db86a1b2eeb59357
faiz ahnaf samudra azis <fzhnf@root533.premium-rootserver.net>
2023-11-08 01:52:29 +0800

    changes: if else konvensional jadi switch case meribetkan GimmickUtils

--- app/src/main/java/org/gnulag/xplora/App.java
+++ app/src/main/java/org/gnulag/xplora/App.java
@@ -9,28 +9,28 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
-  @Override
-  public void start(Stage primaryStage) throws Exception {
-    // Create a label with the "Hello World!" text
-    try {
-      FXMLLoader board = new FXMLLoader(App.class.getResource("/fxml/main.fxml"));
-      Parent root = board.load();
-      Controller controller = board.getController();
-      // Create a scene and set it on the stage
-      Scene scene = new Scene(root);
-      primaryStage.setScene(scene);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Create a label with the "Hello World!" text
+        try {
+            FXMLLoader board = new FXMLLoader(App.class.getResource("/fxml/main.fxml"));
+            Parent root = board.load();
+            Controller controller = board.getController();
+            // Create a scene and set it on the stage
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
 
-      // Set the title of the application window
-      primaryStage.setTitle("Xplora");
+            // Set the title of the application window
+            primaryStage.setTitle("Xplora");
 
-      // Show the application window
-      primaryStage.show();
+            // Show the application window
+            primaryStage.show();
 
-    } catch (Exception e) {
-      e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-  }
-  public static void main(String[] args) {
+    public static void main(String[] args) {
         launch(args);
-  }
+    }
 }
--- app/src/main/java/org/gnulag/xplora/utils/GimmickUtil.java
+++ app/src/main/java/org/gnulag/xplora/utils/GimmickUtil.java
@@ -3,26 +3,35 @@ package org.gnulag.xplora.utils;
 import java.util.ArrayList;
 import java.util.Random;
 
-/**
- * GimmickUtil
- */
+/** GimmickUtil */
 public class GimmickUtil {
     public static void checkGimmick(ArrayList<String> combinedOutput) {
         // Cek apakah ada hasil pencarian yang mengandung "suit"
         for (String item : combinedOutput) {
-            if (item.toLowerCase().contains("suit")) {
-                // Jika ada, tambahkan gimmick suit
+            switch (item.toLowerCase().contains("suit") ? 0 : 1) {
+                case 0:
+                break;
+                case 1:
                 combinedOutput.add(0, suit());
-                return; // Hanya satu gimmick boleh ditambahkan, sehingga kita keluar dari loop
+                return;
+                // Jika ada, tambahkan gimmick suit
+                // Hanya satu gimmick boleh ditambahkan, sehingga kita keluar dari loop
             }
         }
 
         // Cek apakah ada hasil pencarian yang mengandung "acak" atau "random"
         for (String item : combinedOutput) {
-            if (item.toLowerCase().contains("acak") || item.toLowerCase().contains("random")) {
-                // Jika ada, tambahkan gimmick randomGimmick
+            switch (item.toLowerCase().contains("acak") || item.toLowerCase().contains("random")
+            ? 0
+            : 1) {
+                case 0:
+                break;
+                case 1:
                 combinedOutput.add(0, randomGimmick());
-                return; // Hanya satu gimmick boleh ditambahkan, sehingga kita keluar dari loop
+                return;
+                // Jika ada, tambahkan gimmick randomGimmick
+                // // Hanya satu gimmick boleh ditambahkan, sehingga kita keluar dari
+                // loop
             }
         }
     }
@@ -35,7 +44,7 @@ public class GimmickUtil {
 
     private static String suit() {
         // Implementasi gimmick suit, menghasilkan string acak
-        String[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
         Random random = new Random();
         int randomIndex = random.nextInt(suits.length);
         return "Suit gimmick: " + suits[randomIndex];
