a72cbd0fe3d243c1f7c2f053b888d2326186aff9
faiz ahnaf samudra azis <fzhnf@root533.premium-rootserver.net>
2023-11-11 23:22:23 +0800

    feature cut result to 5 line

--- app/src/main/java/org/gnulag/xplora/controllers/Controller.java
+++ app/src/main/java/org/gnulag/xplora/controllers/Controller.java
@@ -3,48 +3,38 @@ package org.gnulag.xplora.controllers;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
-
-import org.gnulag.xplora.utils.PrintsUtil;
-import org.gnulag.xplora.utils.RandomGimmick;
-import org.gnulag.xplora.models.RedBlackTreeMap;
-import org.gnulag.xplora.utils.GameGimmick;
-import org.gnulag.xplora.utils.JSONUtil;
-
+import javafx.animation.TranslateTransition;
+import javafx.concurrent.Worker;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-import javafx.fxml.Initializable;
-import javafx.animation.TranslateTransition;
-import javafx.event.ActionEvent;
-import javafx.util.Duration;
-import javafx.concurrent.Worker;
-
 import javafx.scene.web.WebView;
+import javafx.util.Duration;
+import org.gnulag.xplora.models.RedBlackTreeMap;
+import org.gnulag.xplora.utils.GameGimmick;
+import org.gnulag.xplora.utils.JSONUtil;
+import org.gnulag.xplora.utils.PrintsUtil;
+import org.gnulag.xplora.utils.RandomGimmick;
+
 public class Controller implements Initializable {
+    @FXML private ListView<String> listView;
 
-    @FXML
-    private ListView<String> listView;
+    @FXML private TextField searchBar;
 
-    @FXML
-    private TextField searchBar;
+    @FXML private Label searchButton;
 
-    @FXML
-    private Label searchButton;
+    @FXML private AnchorPane slider;
 
-    @FXML
-    private AnchorPane slider;
-    
-    @FXML
-    private VBox textAreaContainer;
-    @FXML
-    private Label backButton;
+    @FXML private VBox textAreaContainer;
+    @FXML private Label backButton;
 
-    @FXML
-    private TextArea description;
+    @FXML private TextArea description;
 
     private RedBlackTreeMap<String, String> rbTree;
 
@@ -63,92 +53,102 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        searchButton.setOnMouseClicked(event -> {
-            String searchParam = searchBar.getText().toLowerCase();
-            if (!searchParam.isEmpty()) {
-                List<String> results = PrintsUtil.printRedBlackTreeResults(rbTree, searchParam);
+        searchButton.setOnMouseClicked(
+            event -> {
+                String searchParam = searchBar.getText().toLowerCase();
+                if (!searchParam.isEmpty()) {
+                    List<String> results = PrintsUtil.printRedBlackTreeResults(rbTree, searchParam);
 
-                // Clear previous results
-                listView.getItems().clear();
+                    // Clear previous results
+                    listView.getItems().clear();
 
-                // Display new results in the ListView
-                for (String result : results) {
-                    String[] resultSplit = result.split("\n");
-                    String cutResult = "";
-                    for (int i = 0; i < 5; i++) {
-                            cutResult += resultSplit[i];
+                    // Display new results in the listView
+                    for (String result : results) {
+                        String[] resultSplit = result.split("\n");
+                        int resultSplitLength = resultSplit.length;
+                        System.out.println(resultSplitLength);
+                        String cutResult = "";
+                        int cutLength = 5 > resultSplitLength ? resultSplitLength : 5;
+                        for (int i = 0; i < cutLength; i++) {
+                            cutResult += resultSplit[i] + "\n";
+                        }
+                        listView.getItems().add(cutResult);
                     }
-                    listView.getItems().add(cutResult);
                 }
-            }
-        });
-
+            });
         slider.setTranslateX(400);
-        listView.setOnMouseClicked(event -> {
-            String selectedItem = listView.getSelectionModel().getSelectedItem();
-            if (selectedItem != null) {
-                String contents = selectedItem;
-                textAreaContainer.getChildren().clear();
+        listView.setOnMouseClicked(
+            event -> {
+                String selectedItem = listView.getSelectionModel().getSelectedItem();
+                if (selectedItem != null) {
+                    String contents = selectedItem;
+                    textAreaContainer.getChildren().clear();
 
-                WebView webView = new WebView();
-                String content = "judul: " + contents ;
-                webView.getEngine().loadContent(content);
-                applyTextHighlight(webView, searchBar.getText());
-                textAreaContainer.getChildren().add(webView);
+                    WebView webView = new WebView();
+                    String content = "judul: " + contents;
+                    webView.getEngine().loadContent(content);
+                    applyTextHighlight(webView, searchBar.getText());
+                    textAreaContainer.getChildren().add(webView);
 
-                // searchBar.clear(); 
+                    // searchBar.clear();
 
+                    TranslateTransition slide = new TranslateTransition();
+                    slide.setDuration(Duration.seconds(0.5));
+                    slide.setNode(slider);
 
+                    slide.setToX(0);
+                    slide.play();
+
+                    slider.setTranslateX(400);
+
+                    slide.setOnFinished(
+                    (ActionEvent e) -> {
+                            listView.setVisible(true);
+                            backButton.setVisible(true);
+                        });
+                }
+            });
+
+        backButton.setOnMouseClicked(
+            event -> {
                 TranslateTransition slide = new TranslateTransition();
                 slide.setDuration(Duration.seconds(0.5));
                 slide.setNode(slider);
 
-                slide.setToX(0);
+                slide.setToX(400);
                 slide.play();
 
-                slider.setTranslateX(400);
+                slider.setTranslateX(0);
 
-                slide.setOnFinished((ActionEvent e) -> {
-                    listView.setVisible(true);
-                    backButton.setVisible(true);
-                });
-            }
-        });
-
-
-        backButton.setOnMouseClicked(event -> {
-            TranslateTransition slide = new TranslateTransition();
-            slide.setDuration(Duration.seconds(0.5));
-            slide.setNode(slider);
-
-            slide.setToX(400);
-            slide.play();
-
-            slider.setTranslateX(0);
-
-            slide.setOnFinished((ActionEvent e) -> {
-                listView.setVisible(true);
-                backButton.setVisible(false);
+                slide.setOnFinished(
+                (ActionEvent e) -> {
+                        listView.setVisible(true);
+                        backButton.setVisible(false);
+                    });
             });
-        });
     }
 
     private void applyTextHighlight(WebView webView, String inputText) {
-        webView.getEngine().getLoadWorker().stateProperty().addListener(
+        webView
+            .getEngine()
+            .getLoadWorker()
+            .stateProperty()
+            .addListener(
             (observable, oldValue, newValue) -> {
-                if (newValue == Worker.State.SUCCEEDED) {
-                    String content = webView.getEngine().getDocument().getDocumentElement().getTextContent();
+                    if (newValue == Worker.State.SUCCEEDED) {
+                        String content =
+                        webView.getEngine().getDocument().getDocumentElement().getTextContent();
 
-                    // Menyamakan warna bold dan gaya teks
-                    content = content.replaceAll(inputText, String.format("<b>%s</b>", inputText));
+                        // Menyamakan warna bold dan gaya teks
+                        content = content.replaceAll(inputText, String.format("<b>%s</b>", inputText));
 
-                    // Mengatur teks dengan gaya khusus ke dalam WebView
-                    webView.getEngine().loadContent(content);
-                    webView.getEngine().setUserStyleSheetLocation(getClass().getResource("/css/styles.css").toString());
-                }
-            }
-        );
+                        // Mengatur teks dengan gaya khusus ke dalam WebView
+                        webView.getEngine().loadContent(content);
+                        webView
+                            .getEngine()
+                            .setUserStyleSheetLocation(
+                                getClass().getResource("/css/styles.css").toString());
+                    }
+                });
     }
 }
-
-
