09f1cd470eaed6c8ad8c9768d97d4c8b01e46541
dionp3 <donpr2003@gmail.com>
2023-11-13 22:12:59 +0800

    Update GUI

--- app/src/main/java/org/gnulag/xplora/App.java
+++ app/src/main/java/org/gnulag/xplora/App.java
@@ -17,7 +17,6 @@ public class App extends Application {
 
             stage.setTitle("XPLORA");
             stage.getIcons().add(new Image("/image/icon.png"));
-            stage.setScene(new Scene(root, 1068, 715));
             stage.setResizable(false);
             stage.setScene(scene);
             stage.show();
