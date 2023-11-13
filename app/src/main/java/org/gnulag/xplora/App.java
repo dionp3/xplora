0333038dad6880b2932d53225e30de7fbeb872f7
dionp3 <donpr2003@gmail.com>
2023-11-13 14:39:14 +0800

    GUI Enhancement, add icon app, make window not resizable

--- app/src/main/java/org/gnulag/xplora/App.java
+++ app/src/main/java/org/gnulag/xplora/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -14,6 +15,9 @@ public class App extends Application {
             Parent root = loader.load();
             Scene scene = new Scene(root);
 
+            stage.setTitle("XPLORA");
+            stage.getIcons().add(new Image("/image/icon.png"));
+            stage.setResizable(false);
             stage.setScene(scene);
             stage.show();
         } catch(Exception e) {
